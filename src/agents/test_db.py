b0067b4cfad32d9649e9ea20f122b3b10ffc1f93
Mohamed Hossam <80326979+mhdhossam@users.noreply.github.com>
2026-07-31 22:49:33 +0300

    DELETE SRC

--- src/agents/test_db.py
+++
@@ -1,7 +0,0 @@
-import psycopg, os
-from dotenv import load_dotenv
-print("CWD:", os.getcwd())
-loaded =load_dotenv()
-print("load_dotenv found a file:", loaded)
-conn = psycopg.connect(os.getenv("POSTGRES_URL"))
-print("Connected!")
